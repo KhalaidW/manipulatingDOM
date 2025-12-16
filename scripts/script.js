383466b25942b51fc557c6d6af85fad0dcd2cbc6
KhalaidW <walidkhalaid@gmail.com>
2025-12-16 18:14:56 -0500

    lab

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+
+const mainEl = document.querySelector('main')
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
+mainEl.innerHTML = `<h1>DOM Manipulation</h1>`
+
+mainEl.classList.add('flex-ctr')
+
+const topMenuEl = document.getElementById("top-menu")
+
+topMenuEl.style.height = '100%'
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+
+topMenuEl.classList.add('flex-around')
+
+for (let link of menuLinks) {
+    
+    let anchor = document.createElement("a")
+
+    anchor.setAttribute("href", link.href)
+
+    anchor.textContent = link.text
+
+    topMenuEl.appendChild(anchor)
+}
